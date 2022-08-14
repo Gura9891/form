a55f90afb4c1e912b8dfdbd40bd5391ed89c1450
Giang <giang.nguyen090891@gmail.com>
2022-08-15 00:58:55 +0700

    done

--- .history/src/Store/config_20220720195947.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers, createStore} from "redux"
-import * as reducers from './reducers'
-
-const  rootReducer = combineReducers({
-    //* key và value giống nhau thì bỏ key luôn
-    ...reducers,
-});
-
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
--- .history/src/Store/config_20220815005724.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers, createStore} from "redux"
-import * as reducers from './reducers'
-
-const  rootReducer = combineReducers({
-    ...reducers,
-});
-
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
